9639e979141e8b9d832abfafa5131fc17e846bf3
Evgeniy <egeniygal@gmail.com>
2023-03-02 21:48:29 +0200

    Complete

--- src/main/java/edu/goit/Main.java
+++ src/main/java/edu/goit/Main.java
@@ -1,7 +1,18 @@
 package edu.goit;
 
+import com.google.gson.Gson;
+import edu.goit.user.User;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        User user = new User();
+        user.setName("Yevgen");
+        user.setLastName("Galamaga");
+
+        Gson gson = new Gson();
+
+        System.out.println(gson.toJson(user));
+
     }
 }
